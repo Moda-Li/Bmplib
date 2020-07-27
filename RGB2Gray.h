aa0ca4e5465bb849c04dafb1b400690fdff33065
lihaoyu <374426919@qq.com>
2020-07-27 10:55:57 +0800

    修改头文件注释规范

--- RGB2Gray.h
+++ RGB2Gray.h
@@ -1,8 +1,37 @@
 #ifndef _RGB2GRAY_H_
 #define _RGB2GRAY_H_
 
-void RGB2GrayTypical(unsigned char *GrayImg, int GrayImgSizePix, unsigned char *SrcImage, int SrcImgSizePix);
-void RGB2GrayInPhotoShop(unsigned char *GrayImg, int GrayImgSizePix, unsigned char *SrcImage, int SrcImgSizePix);
-void RGB2GrayAverage(unsigned char *GrayImg, int GrayImgSizePix, unsigned char *SrcImage, int SrcImgSizePix);
+/**
+ * 功能：RGB图像转灰度图
+ * 参数：
+ *      GrayImg：灰度图数据缓存区
+ *      GrayImgSizePix：灰度图数据缓存区大小(单位：像素)
+ *      SrcImage：RGB图源数据
+ *      SrcImgSizePix：RGB图源数据大小(单位：像素)
+ * 原理：使用通用算法计算
+*/
+extern void RGB2GrayTypical(unsigned char *GrayImg, int GrayImgSizePix, unsigned char *SrcImage, int SrcImgSizePix);
+
+/**
+ * 功能：RGB图像转灰度图
+ * 参数：
+ *      GrayImg：灰度图数据缓存区
+ *      GrayImgSizePix：灰度图数据缓存区大小(单位：像素)
+ *      SrcImage：RGB图源数据
+ *      SrcImgSizePix：RGB图源数据大小(单位：像素)
+ * 原理：使用PhotoShop原理计算
+*/
+extern void RGB2GrayInPhotoShop(unsigned char *GrayImg, int GrayImgSizePix, unsigned char *SrcImage, int SrcImgSizePix);
+
+/**
+ * 功能：RGB图像转灰度图
+ * 参数：
+ *      GrayImg：灰度图数据缓存区
+ *      GrayImgSizePix：灰度图数据缓存区大小(单位：像素)
+ *      SrcImage：RGB图源数据
+ *      SrcImgSizePix：RGB图源数据大小(单位：像素)
+ * 原理：使用均值计算
+*/
+extern void RGB2GrayAverage(unsigned char *GrayImg, int GrayImgSizePix, unsigned char *SrcImage, int SrcImgSizePix);
 
 #endif
